c17ce65d66e66fb06eee698b7089353077801e9b
rgorostiague <rgorostiague@sisptandil.gob.ar>
2023-09-20 12:53:00 -0300

    adicion del header, footer y componente main, fuentes

--- Tpe-RolePLay/src/app/app.module.ts
+++ Tpe-RolePLay/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainComponent } from './shared/components/pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
