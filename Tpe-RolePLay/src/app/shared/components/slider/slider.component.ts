df7b521f37ca80c340e750e400794011db5bf912
Javier <javoalon@gmail.com>
2023-10-09 21:30:40 -0300

    Fix rutas de imagenes y href

--- Tpe-RolePLay/src/app/shared/components/slider/slider.component.ts
+++ Tpe-RolePLay/src/app/shared/components/slider/slider.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SliderComponent {
   images = [
-    { url: 'assets/images/slider/4-en-linea-banner.png', alt: '4 en linea', href:'/4-en-linea' },
-    { url: 'assets/images/slider/gtav-banner.jpg', alt: 'gta5', href:'/gta-5'},
-    { url: 'assets/images/slider/fc24-banner.png', alt: 'fc24' , href:'/fc24'},
+    { url: 'assets/images/slider/4-en-linea-banner.png', alt: '4 en linea', href:'/Tpe-Interfases-Tudai2023/4-en-linea' },
+    { url: 'assets/images/slider/gtav-banner.jpg', alt: 'gta5', href:'/Tpe-Interfases-Tudai2023/gta-5'},
+    { url: 'assets/images/slider/fc24-banner.png', alt: 'fc24' , href:'/Tpe-Interfases-Tudai2023/fc24'},
   ];
 }
